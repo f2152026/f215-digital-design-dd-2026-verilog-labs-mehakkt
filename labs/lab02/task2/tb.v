6704d334d4734188abece0a267afae207dcc257a
Mehakkt <f20250112@pilani.bits-pilani.ac.in>
2026-09-15 17:57:09 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,27 +1,43 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
 
 module tb;
 
-  // TODO: declare the inputs and outputs
+  reg  [1:0] t_sel;
+  wire [7:0] t_dout;
 
-  // TODO: instantiate DUT here
+  // Instantiate LUT
+  lut U1 (
+    .sel(t_sel),
+    .dout(t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
+
   string vcd_file;
+
   initial begin
+
     if ($value$plusargs("vcd=%s", vcd_file)) begin
+
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+
+      $dumpvars(0, tb);
+
     end
+
   end
 
   initial begin
-    // TODO: apply different input combinations
+
+    t_sel = 2'b00;
+    #5 t_sel = 2'b01;
+    #5 t_sel = 2'b10;
+    #5 t_sel = 2'b11;
+    #5 $finish;
 
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%b | dout=%b", t_sel, t_dout);
 
 endmodule
